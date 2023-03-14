159c293ddb7be12982cc9839007dd49ae4ef2fc0
Jeff DeCola (TK2-PC) <jeffdecola@gmail.com>
2023-03-14 17:58:19 -0400

    updating ci pipeline

--- ci/destroy-pipeline-only.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# crypto-miner-manager destroy-pipeline.sh
-
-echo " "
-echo "Destroy pipeline on target jeffs-ci-target which is team jeffs-ci-team"
-fly --target jeffs-ci-target \
-    destroy-pipeline \
-    --pipeline crypto-miner-manager-deploy
-echo " "
