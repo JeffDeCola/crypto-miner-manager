9009111cbc331ec5b06e78b7a9040bd61e96cac8
Jeff DeCola (TK2-PC) <jeffdecola@gmail.com>
2023-03-14 00:18:30 -0400

    testing ci docker deploy on local machine

---
+++ ci/destroy-pipeline-only.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# crypto-miner-manager destroy-pipeline.sh
+
+echo " "
+echo "Destroy pipeline on target jeffs-ci-target which is team jeffs-ci-team"
+fly --target jeffs-ci-target \
+    destroy-pipeline \
+    --pipeline crypto-miner-manager-deploy
+echo " "
